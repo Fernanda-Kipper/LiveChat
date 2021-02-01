cbca0e5c18152398ad1c2dee738fd0ca4e8653a3
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-02-01 03:11:19 -0300

    organize env variables

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCR9vpZpC4KELHzRtf3_uCd7PLnTcXhshk",
-  authDomain: "realtimechat-c2cf7.firebaseapp.com",
-  projectId: "realtimechat-c2cf7",
-  storageBucket: "realtimechat-c2cf7.appspot.com",
-  messagingSenderId: "276136820376",
-  appId: "1:276136820376:web:fc448581aed69000d8daed",
-  measurementId: "G-D119Y7R7GY"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASURUMENT
 })
 
 const auth = firebase.auth();
