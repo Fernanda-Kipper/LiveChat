e590a8d3c01da3a6db8649bc9edcd4c2593b9e18
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-02-01 02:53:17 -0300

    test key

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp(process.env.FIREBASECONFIG)
+firebase.initializeApp({
+  apiKey: "AIzaSyCR9vpZpC4KELHzRtf3_uCd7PLnTcXhshk",
+  authDomain: "realtimechat-c2cf7.firebaseapp.com",
+  projectId: "realtimechat-c2cf7",
+  storageBucket: "realtimechat-c2cf7.appspot.com",
+  messagingSenderId: "276136820376",
+  appId: "1:276136820376:web:fc448581aed69000d8daed",
+  measurementId: "G-D119Y7R7GY"
+})
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
