f4cc68fe16cd09f44664c8ef4bd3e66de8a0e0a0
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-02-01 14:35:55 -0300

    add all react app envs

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 
 firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASURUMENT
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURUMENT
 })
 
 export default firebase
